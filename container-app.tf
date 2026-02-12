4b24d19521b418fd2ace65f8ad69eebc9538d0d9
David Gardiner <david@gardinerfamily.au>
2026-02-12 22:12:22 +1030

    Container apps

---
+++ container-app.tf
@@ -0,0 +1,30 @@
+resource "azurerm_container_app_environment" "example" {
+  name                       = "cae-postgresql-apps-australiaeast"
+  location                   = data.azurerm_resource_group.rg.location
+  resource_group_name        = data.azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+  logs_destination           = "log-analytics"
+  public_network_access      = "Enabled"
+  infrastructure_subnet_id   = azurerm_subnet.containerapp.id
+  workload_profile {
+    name                  = "Workload"
+    workload_profile_type = "Consumption"
+  }
+}
+
+resource "azurerm_container_app" "example" {
+  name                         = "ca-postgresql-apps-australiaeast"
+  container_app_environment_id = azurerm_container_app_environment.example.id
+  resource_group_name          = data.azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+  workload_profile_name        = "Workload"
+
+  template {
+    container {
+      name   = "examplecontainerapp"
+      image  = "mcr.microsoft.com/k8se/quickstart:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+}

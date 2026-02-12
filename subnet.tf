82ec76384e2335df1e1f39be61adfde2a0087fc9
David Gardiner <david@gardinerfamily.au>
2026-02-12 16:07:30 +1030

    Add subnet

---
+++ subnet.tf
@@ -0,0 +1,16 @@
+resource "azurerm_subnet" "example" {
+  name                 = "subnet-postgresql-apps-australiaeast"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}

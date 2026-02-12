83be6b1b6c7773cc58ff1614270faf5795077197
David Gardiner <david@gardinerfamily.au>
2026-02-13 09:20:48 +1030

    Set infrastructure_resource_group_name

--- container-app.tf
+++ container-app.tf
@@ -1,37 +1,44 @@
 resource "azurerm_container_app_environment" "example" {
-  name                       = "cae-postgresql-apps-australiaeast"
-  location                   = data.azurerm_resource_group.rg.location
-  resource_group_name        = data.azurerm_resource_group.rg.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
-  logs_destination           = "log-analytics"
-  public_network_access      = "Enabled"
-  infrastructure_subnet_id   = azurerm_subnet.containerapp.id
-}
+  name                               = "cae-postgresql-apps-australiaeast"
+  location                           = data.azurerm_resource_group.rg.location
+  resource_group_name                = data.azurerm_resource_group.rg.name
+  log_analytics_workspace_id         = azurerm_log_analytics_workspace.example.id
+  logs_destination                   = "log-analytics"
+  public_network_access              = "Enabled"
+  infrastructure_subnet_id           = azurerm_subnet.containerapp.id
+  infrastructure_resource_group_name = "rg-postgresql-apps-infra-australiaeast" # Set this explicitly, otherwise Azure will create a new resource group with a random name and you will need to tell Terraform to ignore it
 
-resource "azurerm_container_app" "example" {
-  name                         = "ca-postgresql-apps-australiaeast"
-  container_app_environment_id = azurerm_container_app_environment.example.id
-  resource_group_name          = data.azurerm_resource_group.rg.name
-  revision_mode                = "Single"
-
-  template {
-    container {
-      name   = "examplecontainerapp"
-      image  = "mcr.microsoft.com/dotnet/samples:aspnetapp-chiseled"
-      cpu    = 0.25
-      memory = "0.5Gi"
-    }
-  }
-
-  ingress {
-    transport                  = "http"
-    target_port                = 443
-    exposed_port = 8080
-    allow_insecure_connections = false
-    external_enabled           = true
-    traffic_weight {
-      percentage      = 100
-      latest_revision = true
-    }
+  workload_profile {
+    name                  = "Consumption"
+    workload_profile_type = "Consumption"
+    minimum_count         = 0
+    maximum_count         = 0
   }
 }
+
+# resource "azurerm_container_app" "example" {
+#   name                         = "ca-postgresql-apps-australiaeast"
+#   container_app_environment_id = azurerm_container_app_environment.example.id
+#   resource_group_name          = data.azurerm_resource_group.rg.name
+#   revision_mode                = "Single"
+
+#   template {
+#     container {
+#       name   = "examplecontainerapp"
+#       image  = "mcr.microsoft.com/dotnet/samples:aspnetapp-chiseled"
+#       cpu    = 0.25
+#       memory = "0.5Gi"
+#     }
+#   }
+
+#   ingress {
+#     transport                  = "http"
+#     target_port                = 8080
+#     allow_insecure_connections = false
+#     external_enabled           = true
+#     traffic_weight {
+#       percentage      = 100
+#       latest_revision = true
+#     }
+#   }
+# }

adaa621eccb8ea6c6b5df4b9056690cd3b30c0af
David Gardiner <david@gardinerfamily.au>
2026-02-12 16:12:57 +1030

    Private DNS Zone

---
+++ dns.tf
@@ -0,0 +1,12 @@
+resource "azurerm_private_dns_zone" "example" {
+  name                = "example.postgres.database.azure.com"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "example" {
+  name                  = "exampleVnetZone.com"
+  private_dns_zone_name = azurerm_private_dns_zone.example.name
+  virtual_network_id    = azurerm_virtual_network.example.id
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  depends_on            = [azurerm_subnet.example]
+}

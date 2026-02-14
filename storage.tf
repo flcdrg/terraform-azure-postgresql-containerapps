5ec6a220138bcacee7d06723cae5e66c147753d0
David Gardiner <david@gardinerfamily.au>
2026-02-14 17:41:29 +1030

    Allow storage VNET

--- storage.tf
+++ storage.tf
@@ -5,12 +5,12 @@ resource "random_string" "storage_suffix" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "stdirectus${random_string.storage_suffix.result}aue"
-  resource_group_name      = data.azurerm_resource_group.rg.name
-  location                 = data.azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  public_network_access_enabled = false
+  name                          = "stdirectus${random_string.storage_suffix.result}aue"
+  resource_group_name           = data.azurerm_resource_group.rg.name
+  location                      = data.azurerm_resource_group.rg.location
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
+  public_network_access_enabled = true
 }
 
 resource "azurerm_storage_account_network_rules" "storage" {

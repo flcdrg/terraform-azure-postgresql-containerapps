ebae663a7dd14837f3bc4012ee44ab621061c99b
David Gardiner <david@gardinerfamily.au>
2026-02-28 11:19:21 +1030

    Add mdsnippet comments

--- postgresql.tf
+++ postgresql.tf
@@ -1,8 +1,10 @@
+
+# begin-snippet: PostgreSQLServer
 resource "azurerm_postgresql_flexible_server" "server" {
   name                              = "psql-postgresql-apps-australiaeast"
   resource_group_name               = data.azurerm_resource_group.rg.name
   location                          = data.azurerm_resource_group.rg.location
-  version                           = "11" # "17"
+  version                           = "11"
   delegated_subnet_id               = azurerm_subnet.example.id
   private_dns_zone_id               = azurerm_private_dns_zone.example.id
   public_network_access_enabled     = false
@@ -17,6 +19,7 @@ resource "azurerm_postgresql_flexible_server" "server" {
   sku_name   = "B_Standard_B1ms"
   depends_on = [azurerm_private_dns_zone_virtual_network_link.example]
 }
+# end-snippet
 
 resource "azurerm_postgresql_flexible_server_database" "db" {
   name      = "directus"
